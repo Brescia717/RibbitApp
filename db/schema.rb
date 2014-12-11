60b6ed380f5c75be22f57bd11160c2fbf8e4c607
Paul Brescia <brescia717@gmail.com>
2014-12-11 10:08:46 -0500

    Relationships need to be redirected correctly

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209200551) do
+ActiveRecord::Schema.define(version: 20141210201553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id", null: false
+    t.integer  "followed_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ribbits", force: true do |t|
     t.text     "content",    null: false
