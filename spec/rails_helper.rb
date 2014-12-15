2aa6ed3281baa6c28192189d543cdfa71f7f34b9
Paul Brescia <brescia717@gmail.com>
2014-12-15 11:57:15 -0500

    fixed rspec not running issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require 'capybara/rails'
-require 'factory_girl'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
+require 'capybara/rails'
+require 'factory_girl'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
